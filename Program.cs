17492ca97a0b4d51de8ff50ebbbc7b972c4d10bd
Matthew Luchini <luch@Matthews-MBP.lan>
2023-02-01 13:02:00 -0500

    methods1and2

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿class Program
+{
+
+    public static int Sum(int num1, int num2)
+    {
+        var answer = num1 + num2;
+        return answer;
+    }
+
+    public static int Multiply(int num1, int num2, int num3)
+    {
+        return num1 * num2 * num3;
+    }
+
+    static void Main(string[] args)
+    {
+
+        var amountOfCars = Sum(2, 6);
+        var blah = Multiply(60, 2, 4);
+
+        //Exercise 1
+
+        Console.WriteLine("What is your first name?");
+        var userName = Console.ReadLine();
+
+        Console.WriteLine($"Hi,{userName}. What is your favorite color? ");
+        var color = Console.ReadLine();
+
+        Console.WriteLine($"{color} is an awesome color! What is your favorite animal?");
+        var animal = Console.ReadLine();
+
+        Console.WriteLine("Great! Now, what is your favorite band?");
+        var band = Console.ReadLine();
+
+        Console.WriteLine($"Name: {userName}");
+        Console.WriteLine($"Favorite color: {color}");
+        Console.WriteLine($"Favorite animal: {animal}");
+        Console.WriteLine($"Favorite band: {band}");
+
+    }
+}
+    
+ 
